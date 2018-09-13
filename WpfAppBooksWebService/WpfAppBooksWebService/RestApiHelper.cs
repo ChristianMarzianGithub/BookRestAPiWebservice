667b24a9081af30673f580bb076af6a51c5bab07
randwerArbeit <marzian@innosoft.de>
2018-09-13 10:57:43 +0200

    initial commit

---
+++ WpfAppBooksWebService/WpfAppBooksWebService/RestApiHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfAppBooksWebService
+{
+    class RestApiHelper
+    {
+        public void HelloWorld()
+        {
+            MessageBox.Show("Hello World");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
